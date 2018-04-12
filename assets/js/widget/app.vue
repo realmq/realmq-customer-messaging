d47beb46b9d59dff0aa1492b2ea1ec570c72b31d
Alrik Zachert <az@kreatiefer.de>
2018-04-12 02:00:52 +0200

    Slice out chat functionality and refactore widget styles.

--- assets/js/widget/app.vue
+++ assets/js/widget/app.vue
@@ -1,67 +1,64 @@
 <template>
   <div class="rmq-widget">
     <transition name="rmq-fade-in">
-      <div v-if="isOpen" class="rmq-widget-chat">
-        <nav class="rmq-widget-nav">
-          <div class="rmq-default-nav">
-            <div class="rmq-main-nav">
-              <div class="rmq-nav-toggle"></div>
-              <div class="rmq-main-nav-item">
-                <a class="rmq-main-nav-item-link" href="#">Alrik</a>
+      <div v-if="isOpen" class="rmq-widget-window">
+        <div class="rmq-widget-nav">
+          <div class="rmq-nav-toggle"></div>
+          <div class="rmq-main-nav-item">
+            <a class="rmq-main-nav-item-link" href="#">Alrik</a>
+          </div>
+          <div class="rmq-nav-options" v-on:click="isMenuOpen = !isMenuOpen">
+            <transition name="rmq-fade-in">
+              <div v-if="isMenuOpen" class="rmq-widget-menu">
+                <a @click="isOpen = false" class="rmq-widget-menu-item">Close</a>
               </div>
-              <div class="rmq-nav-options"></div>
-            </div>
+            </transition>
           </div>
-        </nav>
+        </div>
         <div class="rmq-widget-body">
-          <div class="rmq-widget-content">
-            <div class="rmq-message-wrapper rmq-is-them">
-              <div class="rmq-circle-wrapper animated bounceIn"></div>
-              <div class="rmq-text-wrapper animated fadeIn">Howdy!</div>
-            </div>
-            <div class="rmq-message-wrapper rmq-is-them">
-              <div class="rmq-circle-wrapper rmq-is-animated rmq-is-bounceIn"></div>
-              <div class="rmq-text-wrapper rmq-is-animated rmq-is-fadeIn">How can we support you?</div>
-            </div>
-            <div class="rmq-message-wrapper rmq-is-me">
-              <div class="rmq-circle-wrapper rmq-is-animated rmq-is-bounceIn"></div>
-              <div class="rmq-text-wrapper rmq-is-animated rmq-is-fadeIn">Can I install RealMQ on my own hardware?</div>
-            </div>
-          </div>
+          <widget-chat :realmq="realmq"></widget-chat>
         </div>
-        <div class="rmq-widget-bottom">
-          <textarea class="rmq-widget-input"></textarea>
-          <div class="rmq-widget-send"></div>
-        </div>
+        <div class="rmq-widget-footer"></div>
       </div>
     </transition>
-    <div v-bind:class="{'rmq-is-open': isOpen}" v-on:click="isOpen = !isOpen" class="rqm-widget-toggle"></div>
+    <div :class="{'rmq-is-open': isOpen}" @click="isOpen = !isOpen" class="rqm-widget-toggle"></div>
   </div>
 </template>
 
 <script>
   var RealMQ = require('../../../../realmq-web-sdk/dist/realmq-0.0.0');
+  var Chat = require('./chat.vue');
 
   var apiClient = new RealMQ.ApiClient(null, { baseUrl: '/' });
 
   module.exports = {
     name: 'app',
+    components: {
+      'widget-chat': Chat,
+    },
     data: function() {
       return {
         session: null,
         isOpen: false,
+        isMenuOpen: false,
+        realmq: null
       };
     },
     created: function() {
-      this.loadSession();
+      this.loadSession().then(this.initializeRealmq.bind(this));
     },
     methods: {
       loadSession: function() {
         var $data = this.$data;
 
-        apiClient.get({ path: 'session' }).then(function(session) {
+        return apiClient.get({ path: 'session' }).then(function(session) {
           $data.session = session;
+
+          return session;
         })
+      },
+      initializeRealmq: function() {
+        this.$data.realmq = new RealMQ(this.session.token);
       }
     }
   };
@@ -74,36 +71,32 @@
     position: fixed;
     right: 2rem;
     bottom: 2rem;
-    font-family: 'Dosis', Helvetica, Arial, sans-serif;
+    font-family: $font-sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  .rmq-widget-chat {
+  .rmq-widget-window {
+    display: flex;
+    flex-direction: column;
     height: 520px;
     width: 320px;
     overflow: hidden;
     background-color: white;
-    box-shadow: 0px 3px 3px 0px rgba(50, 50, 50, 0.5);
     position: relative;
+    @include material-shadow(3);
 
     .rmq-widget-body {
+      flex-grow: 1;
       overflow: scroll;
       height: 520px;
-      padding-top: $navHeight;
+      padding: ($navHeight - ($navHeight/1.5)) / 2;
       background: darken($white, 5%);
 
       -ms-overflow-style: none;
       overflow: -moz-scrollbars-none;
-      //gotta hide windows scrollbars
 
       &::-webkit-scrollbar {
         width: 0 !important
-      }
-
-      .rmq-widget-content {
-        padding: ($navHeight - ($navHeight/1.5)) / 2;
-        position: relative;
-        margin-bottom: $navHeight/2;
       }
     }
 
@@ -111,97 +104,72 @@
   }
 
   .rmq-widget-nav {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
     height: $navHeight;
     z-index: $nav;
+    background-color: $primary;
+    border-bottom: 3px solid darken($primary, 10%);;
+    color: $white;
 
-    .rmq-default-nav {
-      height: $navHeight;
-      width: 100%;
+    @include material-shadow(1);
+
+    .rmq-nav-toggle {
+      height: 32px;
+      width: 32px;
+      background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjQ4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSI0OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPiAgICA8cGF0aCBkPSJNMjAgMTFINy44M2w1LjU5LTUuNTlMMTIgNGwtOCA4IDggOCAxLjQxLTEuNDFMNy44MyAxM0gyMHYtMnoiLz48L3N2Zz4=);
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: $navHeight/2.5;
+      margin: 16px;
+      float: left;
+      cursor: pointer;
+    }
+
+    .rmq-nav-options {
+      height: 32px;
+      width: 32px;
+      background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjQ4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSI0OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPiAgICA8cGF0aCBkPSJNMTIgOGMxLjEgMCAyLS45IDItMnMtLjktMi0yLTItMiAuOS0yIDIgLjkgMiAyIDJ6bTAgMmMtMS4xIDAtMiAuOS0yIDJzLjkgMiAyIDIgMi0uOSAyLTItLjktMi0yLTJ6bTAgNmMtMS4xIDAtMiAuOS0yIDJzLjkgMiAyIDIgMi0uOSAyLTItLjktMi0yLTJ6Ii8+PC9zdmc+);
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: $navHeight/2.5;
+      margin: 16px;
       position: absolute;
-      left: 0;
-      top: 0;
-      z-index: $nav + 10;
+      right: 0;
+      cursor: pointer;
+    }
 
-      background-color: $primary;
-      border-bottom: 3px solid darken($primary, 10%);;
-      color: $white;
+    .rmq-widget-menu {
+      background: $white;
+      position: absolute;
+      top: 1.25rem;
+      right: 1.25rem;
+      color: $black;
+      @include material-shadow(3);
 
-      -webkit-box-shadow: 0px 3px 3px 0px rgba(50, 50, 50, 0.1);
-      -moz-box-shadow:    0px 3px 3px 0px rgba(50, 50, 50, 0.1);
-      box-shadow:         0px 3px 3px 0px rgba(50, 50, 50, 0.1);
+      .rmq-widget-menu-item {
+        padding: 1rem;
+        display: inline-block;
+      }
+    }
 
-      .rmq-main-nav {
-        position: absolute;
-        left: 0;
-        width: 100%;
+    .rmq-main-nav-item {
+      float: left;
+      height: $navHeight;
+      margin-right: 50px;
+      position: relative;
+
+      // text-align: center;
+      line-height: $navHeight;
+
+      .rmq-main-nav-item-link {
+        display: block;
+        position: relative;
         height: $navHeight;
-        top: 0;
+        width: 100%;
 
-        margin: 0;
-        padding: 0;
-
-        list-style: none;
-
-        .rmq-nav-toggle {
-          height: 32px;
-          width: 32px;
-          background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjQ4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSI0OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPiAgICA8cGF0aCBkPSJNMjAgMTFINy44M2w1LjU5LTUuNTlMMTIgNGwtOCA4IDggOCAxLjQxLTEuNDFMNy44MyAxM0gyMHYtMnoiLz48L3N2Zz4=);
-          background-repeat: no-repeat;
-          background-position: center;
-          background-size: $navHeight/2.5;
-          margin: 16px;
-          float: left;
-
-          &:hover {
-            cursor: pointer;
-          }
-        }
-
-        .rmq-nav-options {
-          height: 32px;
-          width: 32px;
-          background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjQ4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSI0OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPiAgICA8cGF0aCBkPSJNMTIgOGMxLjEgMCAyLS45IDItMnMtLjktMi0yLTItMiAuOS0yIDIgLjkgMiAyIDJ6bTAgMmMtMS4xIDAtMiAuOS0yIDJzLjkgMiAyIDIgMi0uOSAyLTItLjktMi0yLTJ6bTAgNmMtMS4xIDAtMiAuOS0yIDJzLjkgMiAyIDIgMi0uOSAyLTItLjktMi0yLTJ6Ii8+PC9zdmc+);
-          background-repeat: no-repeat;
-          background-position: center;
-          background-size: $navHeight/2.5;
-          margin: 16px;
-          position: absolute;
-          right: 0;
-
-          &:hover {
-            cursor: pointer;
-          }
-        }
-
-        .rmq-main-nav-item {
-          float: left;
-          height: $navHeight;
-          margin-right: 50px;
-          position: relative;
-
-          // text-align: center;
-          line-height: $navHeight;
-
-          .rmq-main-nav-item-link {
-            display: block;
-            position: relative;
-            height: $navHeight;
-            width: 100%;
-
-            text-align: center;
-            line-height: $navHeight;
-            text-decoration: none;
-            color: inherit;
-
-            transition: $trans;
-          }
-
-          transition: $trans;
-        }
+        text-align: center;
+        line-height: $navHeight;
+        text-decoration: none;
+        color: inherit;
 
         transition: $trans;
       }
@@ -212,142 +180,6 @@
     transition: $trans;
   }
 
-  .rmq-widget-bottom {
-    position: absolute;
-    bottom: 0; left: 0; right: 0;
-    height: $navHeight;
-    background: $white;
-
-    /* box-shadow: 0px -3px 3px 0px rgba(50, 50, 50, 0.1); */
-
-    .rmq-widget-input {
-      height: $navHeight;
-      background: $white;
-      border: none;
-      width: calc(100% - #{$navHeight});
-      position: absolute;
-      left: 0;
-      top: 0;
-      padding: 0 5%;
-      resize: none;
-      overflow: scroll;
-      padding-top: ($navHeight/2) - 8;
-      font-weight: 300;
-
-      &:focus {
-        outline: none;
-      }
-
-      -ms-overflow-style: none;
-      overflow: -moz-scrollbars-none;
-      //gotta hide windows scrollbars
-
-      &::-webkit-scrollbar {
-        width: 0 !important
-      }
-    }
-
-    .rmq-widget-send {
-      position: absolute;
-      height: $navHeight/1.5;
-      width: $navHeight/1.5;
-      border-radius: 50%;
-      border: 0;
-      background: $primary;
-      color: $white;
-
-      bottom: ($navHeight - ($navHeight/1.5)) / 2;
-      right: ($navHeight - ($navHeight/1.5)) / 2;
-
-      &:before {
-        content: '';
-        background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjQ4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSI0OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTIuMDEgMjFMMjMgMTIgMi4wMSAzIDIgMTBsMTUgMi0xNSAyeiIvPiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PC9zdmc+);
-        background-repeat: no-repeat;
-        background-position: center center;
-        background-size: $navHeight/2.5;
-        position: absolute;
-        top: 0; left: 0; right: 0; bottom: 0;
-      }
-
-      &:focus {
-        outline: none;
-      }
-
-      &:hover {
-        cursor: pointer;
-      }
-    }
-  }
-
-  .rmq-message-wrapper {
-    position: relative;
-    overflow: hidden;
-
-    width: 100%;
-    margin: (($navHeight - ($navHeight/1.5)) / 2) 0;
-    padding: (($navHeight - ($navHeight/1.5)) / 2) 0;
-
-    .rmq-circle-wrapper {
-      height: $navHeight/1.5;
-      width: $navHeight/1.5;
-      border-radius: 50%;
-    }
-
-    .rmq-text-wrapper {
-      padding: ($navHeight - ($navHeight/1.5)) / 2;
-      min-height: $navHeight/1.5;
-      width: 60%;
-      margin: 0 ($navHeight - ($navHeight/1.5)) / 2;
-      box-shadow: 0px 1px 0px 0px rgba(50, 50, 50, 0.3);
-      border-radius: 2px;
-      font-weight: 300;
-      position: relative;
-
-      &:before {
-        content: '';
-        width: 0;
-        height: 0;
-        border-style: solid;
-      }
-    }
-
-    &.rmq-is-them {
-      .rmq-circle-wrapper, .rmq-text-wrapper {
-        background: $primary;
-        float: left;
-        color: $white;
-      }
-
-      .rmq-text-wrapper {
-        &:before {
-          border-width: 0 10px 10px 0;
-          border-color: transparent $primary transparent transparent;
-          position: absolute;
-          top: 0;
-          left: -9px;
-        }
-      }
-    }
-
-    &.rmq-is-me {
-      .rmq-circle-wrapper, .rmq-text-wrapper {
-        background: $white;
-        float: right;
-        color: $black;
-      }
-
-      .rmq-text-wrapper {
-        &:before {
-          border-width: 10px 10px 0 0;
-          border-color: $white transparent transparent transparent;
-          position: absolute;
-          top: 0;
-          right: -9px;
-        }
-      }
-    }
-  }
-
   .rqm-widget-toggle {
     width: 48px;
     height: 48px;
@@ -356,7 +188,7 @@
     background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTIwIDJINGMtMS4xIDAtMiAuOS0yIDJ2MThsNC00aDE0YzEuMSAwIDItLjkgMi0yVjRjMC0xLjEtLjktMi0yLTJ6Ii8+ICAgIDxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48L3N2Zz4=);
     background-position: center;
     background-repeat: no-repeat;
-    box-shadow: 0px 3px 3px 0px rgba(50, 50, 50, 0.5);
+    @include material-shadow(2);
     cursor: pointer;
     margin-top: 1rem;
     float: right;
@@ -381,7 +213,7 @@
       bottom: 1rem;
       right: 1rem;
     }
-    .rmq-widget-chat {
+    .rmq-widget-window {
       position: fixed;
       width: 100%;
       height: auto;
@@ -389,10 +221,6 @@
       left: 0;
       bottom: 5rem;
       transform: translateX(0);
-
-      .rmq-widget-body {
-        height: 100%;
-      }
     }
   }
 </style>
---
+++ assets/js/widget/chat.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="rmq-chat">
+    <template v-if="realmq && isConnected">
+      <div class="rmq-messages">
+        <div class="rmq-message rmq-is-them">
+          <div class="rmq-message-sender"></div>
+          <div class="rmq-message-body">Howdy!</div>
+        </div>
+        <div class="rmq-message rmq-is-them">
+          <div class="rmq-message-sender"></div>
+          <div class="rmq-message-body">How can we support you?</div>
+        </div>
+        <div class="rmq-message rmq-is-me">
+          <div class="rmq-message-sender"></div>
+          <div class="rmq-message-body rmq-is-animated rmq-is-fadeIn">Can I install RealMQ on my own hardware?</div>
+        </div>
+      </div>
+      <div class="rmq-chat-input">
+        <textarea class="rmq-message-input"></textarea>
+        <div class="rmq-message-submit"></div>
+      </div>
+    </template>
+    <template v-else>
+      <div class="rmq-loading">
+        Initialize Chat...
+      </div>
+    </template>
+  </div>
+</template>
+<script>
+  module.exports = {
+    name: "widget-chat",
+    props: {
+      realmq: {
+        required: true,
+      }
+    },
+    data: function() {
+      return {
+        isConnected: false,
+      };
+    },
+    created: function() {
+      var realmq = this.realmq;
+      var $data = this.$data;
+
+      realmq.rtm.connect().then(function() {
+        $data.isConnected = true;
+
+        return realmq.autoSubscribe();
+      });
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  @import "styles/variables";
+
+  .rmq-chat {
+    height: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    color: $black;
+  }
+
+  .rmq-loading {
+    padding-top: 8rem;
+    text-align: center;
+  }
+
+  .rmq-messages {
+    flex-grow: 1;
+    overflow-y: auto;
+    overflow-x: hidden;
+  }
+
+  .rmq-chat-input {
+    background: $white;
+
+    .rmq-message-input {
+      height: $navHeight;
+      background: $white;
+      border: none;
+      width: calc(100% - #{$navHeight});
+      padding: 5%;
+      resize: none;
+      overflow: scroll;
+      padding-top: ($navHeight/2) - 8;
+      font-weight: 300;
+      font-family: $font-sans-serif;
+
+      &:focus {
+        outline: none;
+      }
+
+      -ms-overflow-style: none;
+      overflow: -moz-scrollbars-none;
+
+      &::-webkit-scrollbar {
+        width: 0 !important
+      }
+    }
+
+    .rmq-message-submit {
+      position: absolute;
+      height: $navHeight * 0.625;
+      width: $navHeight * 0.625;
+      border-radius: 50%;
+      border: 0;
+      background: $primary;
+      background-image: url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjQ4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSI0OCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTIuMDEgMjFMMjMgMTIgMi4wMSAzIDIgMTBsMTUgMi0xNSAyeiIvPiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PC9zdmc+);
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: $navHeight * 0.375;
+      color: $white;
+
+      bottom: $navHeight * (1 - 0.625) / 2;
+      right: $navHeight * (1 - 0.625) / 2;
+
+      &:focus {
+        outline: none;
+      }
+
+      cursor: pointer;
+    }
+  }
+
+  .rmq-messages {
+    position: relative;
+    overflow: hidden;
+
+    width: 100%;
+    margin: 1rem 0;
+  }
+
+  .rmq-message-body {
+    padding: 1rem;
+    min-height: $navHeight/1.5;
+    margin: 0 1rem;
+    @include material-shadow(1);
+    border-radius: 2px;
+    font-weight: 300;
+    width: 60%;
+    position: relative;
+
+    &:before {
+      content: '';
+      width: 0;
+      height: 0;
+      border-style: solid;
+    }
+  }
+
+  .rmq-message {
+    display: flex;
+    margin: 1rem;
+
+    .rmq-message-sender {
+      height: $navHeight/1.5;
+      width: $navHeight/1.5;
+      border-radius: 50%;
+    }
+
+    &.rmq-is-them {
+      .rmq-message-sender, .rmq-message-body {
+        background: $primary;
+        color: $white;
+      }
+
+      .rmq-message-body {
+        &:before {
+          border-width: 0 10px 10px 0;
+          border-color: transparent $primary transparent transparent;
+          position: absolute;
+          top: 0;
+          left: -9px;
+        }
+      }
+    }
+
+    &.rmq-is-me {
+      flex-direction: row-reverse;
+
+      .rmq-message-sender, .rmq-message-body {
+        background: $white;
+        color: $black;
+      }
+
+      .rmq-message-body {
+        &:before {
+          border-width: 10px 10px 0 0;
+          border-color: $white transparent transparent transparent;
+          position: absolute;
+          top: 0;
+          right: -9px;
+        }
+      }
+    }
+  }
+</style>
